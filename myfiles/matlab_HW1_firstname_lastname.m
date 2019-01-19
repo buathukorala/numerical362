6ab49efdced3beb32363ddee38602e2f7363b28a
Bhagya Athukorallage <bhagya.athukorala@ttu.edu>
2019-01-18 21:05:25 -0700

    Add files via upload

---
+++ myfiles/matlab_HW1_firstname_lastname.m
@@ -0,0 +1,53 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%         MATLAB HW 1     %%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  - whenever you write code, it is a good practice to 
+%    add comments that describe the code.
+%  - if you use 'two % marks' (%%), we can separate sections
+%    in a script file. 
+%  - good luck!
+
+
+%% Solution - Problem 1 (do not modify this line)
+rho = 700;
+v =     % your turn
+diam_ratio = [0.9, 0.8, 0.7, 0.6, 0.5, 0.2];
+
+deltaP =   % your turn
+
+
+
+
+%% Solution - Problem 2 (do not modify this line)
+
+func2 = @(x)(x.*exp(-0.7.*x).*sqrt(1+x));
+
+x = linspace(0, ,50); % your turn
+y = func2(x);
+
+plot( )  % your turn
+
+
+
+%% Solution - Problem 3 (do not modify this line)
+
+
+
+
+%% Solution - Problem 4 (do not modify this line)
+
+
+
+%% Solution - Problem  5 (do not modify this line)
+
+
+
+
+
+
+
+
+
+
